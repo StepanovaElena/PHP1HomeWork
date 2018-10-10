bd2ce4fb4d289511bfe82cac1c8a7840a1eb0659
StepanovaElena <stepanova_eg@bk.ru>
2018-10-10 19:14:08 +0300

    Незначительные изменения

---
+++ HomeWork_1/1(4).php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lena
+ * Date: 10.10.2018
+ * Time: 19:03
+ */
+$title = 'Home Work. Task 4';
+$currentYear = date('Y');
+?>
+
+<head>
+    <title><?= $title ?></title>
+</head>
+<body>
+<h1><?= $currentYear ?></h1>
+</body>
